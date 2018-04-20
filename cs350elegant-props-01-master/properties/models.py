39f5e85cb321a63eae0cfdb625d23c4ec7783538
jmgstokes <35575742+jmgstokes@users.noreply.github.com>
2018-04-20 15:10:14 -0600

    Add files via upload

---
+++ cs350elegant-props-01-master/properties/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Property(models.Model):
+    prop_type = models.CharField(max_length=48)
+    picture_url = models.CharField(max_length=256)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    nearest_elementary = models.CharField(max_length=256)
+
+
+    zip_code = models.IntegerField(Null=True)
+
+
